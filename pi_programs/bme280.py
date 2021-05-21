7ec5c6b33ece43f6cffa65cae4f95469f7ddfa14
Daniel8942 <80713064+Daniel8942@users.noreply.github.com>
2021-05-21 13:14:02 -0500

    Update bme280.py

--- pi_programs/bme280.py
+++ pi_programs/bme280.py
@@ -12,10 +12,12 @@ def bme():
     for x in range(2):
         bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
     
+    # the number 1013.25 is the global average, may need to update
     bme280.sea_level_pressure = 1013.25
     humid = bme280.relative_humidity
     press = bme280.pressure
     tempC = bme280.temperature
+    #convert to F
     tempF = tempC * (9/5) + 32
 
     #temp in F, humid in %, press in hPa
