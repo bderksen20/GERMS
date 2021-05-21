53530a541212de4d94e273df3de53c38803c0802
Daniel8942 <80713064+Daniel8942@users.noreply.github.com>
2021-05-21 13:11:59 -0500

    Update si1145.py

--- pi_programs/si1145.py
+++ pi_programs/si1145.py
@@ -4,12 +4,15 @@ import SI1145.SI1145 as SI1145
 def si():
     sensor = SI1145.SI1145()
 
+    #get data for visible, IR, and UV light
     vis = sensor.readVisible()
     IR = sensor.readIR()
     UV = sensor.readUV()
     uvIndex = UV / 100.0
     
-    # TODO MAKE FLOAT
+    # Subtract 256 because sensor adds an offset of 256 to this value-see datasheet
+    # Division by 1.47 is a scaling factor conversion between the unit the sensor reports (ADC counts) and irradiance (W/m^2)
+    # This value was determined experimentally, and should be reevaluated
     vis = int((vis - 256) / 1.47)
     
     return vis, IR, uvIndex
