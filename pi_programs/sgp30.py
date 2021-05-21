9835cae354d7d092803c0a5f2276ab1be9fdd826
Daniel8942 <80713064+Daniel8942@users.noreply.github.com>
2021-05-21 13:12:58 -0500

    Update sgp30.py

--- pi_programs/sgp30.py
+++ pi_programs/sgp30.py
@@ -5,7 +5,8 @@ import board
 def sgp():
     i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
     sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
-
+    
+    #check datasheet for more info on the baseline
     sgp30.iaq_init()
     sgp30.set_iaq_baseline(0x8973, 0x8AAE)
     
