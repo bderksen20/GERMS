fa9c7784acb55e74f2eff4cd323393310794a6e1
Daniel8942 <80713064+Daniel8942@users.noreply.github.com>
2021-05-21 13:22:36 -0500

    Update power.py

--- pi_programs/power.py
+++ pi_programs/power.py
@@ -3,11 +3,17 @@ import math
 def power(Vrms_unscaled, Irms_unscaled, freq, phase):
      
     freq = round(freq, 0)
+
+     #since there is an RC LP filter, frequency impacts the voltage and current magnitude
+     # for simplicity, only range from 55 to 65 Hz is accounted for, and at 1 Hz intervals
+     # these values were determined using an ADALM2000 to perform a frequency sweep on the filters
     if freq <= 55.0:
         freq = 55.0
     if freq >= 65.0:
         freq = 65.0
     
+     #scaling factors determined by lookup table for current and voltage
+     #these scaling factors account for current/voltage transformer and the filter
     iLookup = {55.0:86.5699, 56.0:85.9740, 57.0:85.3821, 
                58.0:84.6968, 59.0:84.1138, 60.0:83.3426,
                61.0:82.7689, 62.0:82.1991, 63.0:81.5393, 
@@ -22,10 +28,7 @@ def power(Vrms_unscaled, Irms_unscaled, freq, phase):
     Vscale = vLookup[freq]
     
     
-    #these are at 60hz
-    #Iscale = 89.67
-    #Vscale = 322.166
-    
+    #these are the actual values
     Vrms = Vrms_unscaled * Vscale
     Irms = Irms_unscaled * Iscale
     pf = math.cos(phase)
